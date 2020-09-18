935ef0029bf1e05348c766245aa3b4881dc59c72
viki <vikihp1982@hotmail.com>
2020-09-18 21:30:38 +0200

    lupa funcionando

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,43 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/services/user.service';
-import { ProductService } from 'src/app/services/product.service';
-import { Router } from '@angular/router';
-import { CategoryService } from 'src/app/services/category.service';
- 
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
+import { ProductService } from "src/app/services/product.service";
+import { Router } from "@angular/router";
+import { CategoryService } from "src/app/services/category.service";
+// import {MDCIconButtonToggle} from '@material/icon-button';
+// const iconToggle = new MDCIconButtonToggle(document.querySelector('.mdc-icon-button'));
+
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit{
-  isShow = true;
-  public admins = ['superadmin', 'admin'];
+export class HeaderComponent implements OnInit {
+  public isShow = true;
+  public icon = "search";
+  public admins = ["superadmin", "admin"];
   public categories;
-  toggleDisplay(element, text) {
+  public toggleDisplay() {
     this.isShow = !this.isShow;
-      element.textContent = text;
-      element.disabled = true;
+    if (this.icon === "search") {
+      this.icon = "highlight_off";
+    } else {
+      this.icon = "search";
+    }
   }
   constructor(
     public userService: UserService,
     public productService: ProductService,
     public router: Router,
-    public categoryService: CategoryService,
+    public categoryService: CategoryService
   ) {}
 
-ngOnInit():void{
-this.categoryService.getAll()
-.subscribe(
-  (res)=>{this.categories=res;}
-)
-}
+  ngOnInit(): void {
+    this.categoryService.getAll().subscribe((res) => {
+      this.categories = res;
+    });
+  }
   searchProducts(event: any) {
     const searchValue = event.target.search.value;
-    this.router.navigate(['/products/search', searchValue]);
+    this.router.navigate(["/products/search", searchValue]);
   }
   logout() {
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
     this.userService.setUser({});
   }
 }
-

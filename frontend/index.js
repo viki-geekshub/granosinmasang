b8cccf26f76ff7d53b6a8bb1fedaa0212dafa092
Marcos Ostos <marcos.ostos@gmail.com>
2020-09-28 11:19:35 +0200

    modifica base de datos y htm para que la extension de las img sea dinamica y crea picture source

--- frontend/index.js
+++ frontend/index.js
@@ -5,10 +5,11 @@ const imageminWebp = require('imagemin-webp');
 
 (async () => {
 	
-	await imagemin(['src/assets/images/productsImagesJpg/*.{jpg,png}'], {
+	await imagemin(['src/assets/images/productsImages/*.{jpg,png}'], {
 		destination: 'src/assets/images/productsImages/',
 		plugins: [
 			imageminWebp({
+				lossless: true, // Losslessly encode images
 				quality: 75,
 				resize: {
 					width: 1500,

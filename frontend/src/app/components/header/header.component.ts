09233081a11a7dc3b1c7ff5fea5e73bb700a655f
Marcos Ostos <marcos.ostos@gmail.com>
2020-11-09 18:02:59 +0100

    oculta menu y modifica herobanner

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from "@angular/core";
-import { UserService } from "src/app/services/user.service";
-import { ProductService } from "src/app/services/product.service";
-import { Router } from "@angular/router";
-import { CategoryService } from "src/app/services/category.service";
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 
 })
 export class HeaderComponent implements OnInit {
   public width  = 10;
   public isShow = true;
-  public icon = "search";
-  public transition ='';
-  public admins = ["superadmin", "admin"];
+  public icon = 'search';
+  public transition = '';
+  public admins = ['superadmin', 'admin'];
   public categories;
   public toggleDisplay() {
     this.isShow = !this.isShow;
-    if (this.icon === "search") {
-      this.icon = "highlight_off";
+    if (this.icon === 'search') {
+      this.icon = 'highlight_off';
       this.width = 30;
     } else {
-      this.icon = "search";
+      this.icon = 'search';
       this.width = 10;
     }
   }
@@ -35,8 +35,8 @@ export class HeaderComponent implements OnInit {
   ) {}
 
   onActivate(event) {
-    let scrollToTop = window.setInterval(() => {
-        let pos = window.pageYOffset;
+    const scrollToTop = window.setInterval(() => {
+        const pos = window.pageYOffset;
         if (pos > 0) {
             window.scrollTo(0, pos - 20); // how far to scroll on each step
         } else {
@@ -52,10 +52,10 @@ export class HeaderComponent implements OnInit {
   }
   searchProducts(event: any) {
     const searchValue = event.target.search.value;
-    this.router.navigate(["/products/search", searchValue]);
+    this.router.navigate(['/products/search', searchValue]);
   }
   logout() {
-    localStorage.removeItem("authToken");
+    localStorage.removeItem('authToken');
     this.userService.setUser({});
   }
 }
